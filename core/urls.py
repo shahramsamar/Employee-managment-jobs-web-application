b31f5a819d25e661170bad93741d7ff66bec268c
shahram <shahramsamar2010@gmail.com>
2025-06-26 17:27:33 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from..ems import views
+from ems import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
