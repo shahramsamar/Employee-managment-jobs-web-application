de42ad53ea4fb43fa716ee7e2ec81a433fdf6dee
shahram <shahramsamar2010@gmail.com>
2025-06-26 13:32:44 +0330

    Update admin.py

--- ems/admin.py
+++ ems/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Employee,Job
 
-# Register your models here.
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+        list_display = ['First_name','last_name','gender']
+
+    
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['name']
