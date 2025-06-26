574adbd2c4512fac26a645c27b37d8a7f5583328
shahram <shahramsamar2010@gmail.com>
2025-06-26 13:33:22 +0330

    Update models.py

--- ems/models.py
+++ ems/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    GENDER_CHOICES = [("M",'Male'),
+                      ('F',"Female"),
+                      ("O","Other")]
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=13,blank=True)
+    address = models.TextField()
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES,blank=True)
+    Birthday = models.DateField()
+    joining_date = models.DateField()
+    jobs = models.ManyToManyField("Job",blank=True)
+
+class Job(models.Model):
+    name = models.CharField(max_length=255)  
+      
+    def __str__(self) -> str:
+         return super().__str__()
