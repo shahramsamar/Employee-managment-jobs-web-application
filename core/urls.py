eddd654cfad53196cf82cc1de41fa2a9cfb5ba71
shahram <shahramsamar2010@gmail.com>
2025-06-26 13:32:41 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from..ems import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ems.urls')),
+    path('',views.home,name='home'),
+    path('employees/<int:employee_id>/',views.employee_detail,name='employee_detail'),
+    
 ]
