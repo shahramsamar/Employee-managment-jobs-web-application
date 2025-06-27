a1001197676c8acb13b874de6cc58000e4ef3624
shahram <shahramsamar2010@gmail.com>
2025-06-27 23:16:52 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from ems import views
@@ -24,3 +26,5 @@ urlpatterns = [
     path('employees/<int:employee_id>/',views.employee_detail,name='employee_detail'),
     
 ]
+urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
